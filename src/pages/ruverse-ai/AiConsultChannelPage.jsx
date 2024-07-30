e9d57c7a0c0d75e12531f282eff6fa3e5836a92a
unknown <xognl1124@seoul.ac.kr>
2024-07-30 12:25:16 +0900

    fix path

--- src/pages/ruverse-ai/AiConsultChannelPage.jsx
+++ src/pages/ruverse-ai/AiConsultChannelPage.jsx
@@ -1,17 +1,88 @@
-import { AudioRecorder, LocalUser, UploadInfoModal } from "@components/index";
+// import { AudioRecorder, LocalUser, UploadInfoModal } from "@components/index";
+// import { Box, CircularProgress } from "@mui/material";
+// import { useSelector } from "react-redux";
+// import { useParams } from "react-router-dom";
+
+// const AiConsultChannelPage = () => {
+//   const { uname } = useParams();
+
+//   const src = useSelector((state) => state.aiConsult.audio.src);
+//   console.log(`src change ${src}`);
+
+//   const isLoading = useSelector(
+//     (state) => state.aiConsult.audio.upload.isLoading
+//   );
+
+//   return (
+//     <Box width="100%" height="100vh">
+//       {/* remote user */}
+//       <Box
+//         width="100%"
+//         height="90%"
+//         display="flex"
+//         justifyContent="center"
+//         alignItems="center"
+//       >
+//         {isLoading ? (
+//           <CircularProgress />
+//         ) : (
+//           <Box
+//             component="video"
+//             width="100%"
+//             height="100%"
+//             src={src}
+//             autoPlay
+//           />
+//         )}
+//       </Box>
+
+//       <Box
+//         sx={{
+//           position: "absolute",
+//           right: 0,
+//           bottom: 0 + "10%",
+//           backgroundColor: "blue",
+//           width: { xs: "200px", md: "320px" },
+//           height: { xs: "120px", md: "200px" },
+//         }}
+//       >
+//         <LocalUser />
+//       </Box>
+//       <Box
+//         display="flex"
+//         justifyContent="center"
+//         alignItems="center"
+//         height="10%"
+//         borderTop={1}
+//         borderColor={"#ccc"}
+//       >
+//         <AudioRecorder uname={uname} />
+//       </Box>
+//       <UploadInfoModal />
+//     </Box>
+//   );
+// };
+
+// export default AiConsultChannelPage;
+
+import { AudioRecorder, LocalUser } from "@components/index";
 import { Box, CircularProgress } from "@mui/material";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { clearAudioSrc } from "@store/ai/aiConsultSlice"; // Adjust the import path accordingly
 
 const AiConsultChannelPage = () => {
   const { uname } = useParams();
+  const dispatch = useDispatch();
 
   const src = useSelector((state) => state.aiConsult.audio.src);
-  console.log(`src change ${src}`);
-
   const isLoading = useSelector(
     (state) => state.aiConsult.audio.upload.isLoading
   );
+
+  const handleVideoEnd = () => {
+    dispatch(clearAudioSrc());
+  };
 
   return (
     <Box width="100%" height="100vh">
@@ -32,6 +103,7 @@ const AiConsultChannelPage = () => {
             height="100%"
             src={src}
             autoPlay
+            onEnded={handleVideoEnd}
           />
         )}
       </Box>
@@ -40,7 +112,7 @@ const AiConsultChannelPage = () => {
         sx={{
           position: "absolute",
           right: 0,
-          bottom: 0 + "10%",
+          bottom: "10%",
           backgroundColor: "blue",
           width: { xs: "200px", md: "320px" },
           height: { xs: "120px", md: "200px" },
@@ -58,7 +130,7 @@ const AiConsultChannelPage = () => {
       >
         <AudioRecorder uname={uname} />
       </Box>
-      <UploadInfoModal />
+      {/* <UploadInfoModal /> */}
     </Box>
   );
 };
