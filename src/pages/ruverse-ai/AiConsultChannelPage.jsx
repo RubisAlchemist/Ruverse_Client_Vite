d313a903fc6f44573cc9535d06a1c1031a7efbd6
xognl1124@naver.com <xognl1124@naver.com>
2024-07-30 14:10:25 +0900

    video transition smoother

--- src/pages/ruverse-ai/AiConsultChannelPage.jsx
+++ src/pages/ruverse-ai/AiConsultChannelPage.jsx
@@ -1,58 +1,151 @@
+// import { AudioRecorder, LocalUser } from "@components/index";
+// import { Box } from "@mui/material";
+// import { clearAudioSrc } from "@store/ai/aiConsultSlice"; // Adjust the import path accordingly
+// import { useDispatch, useSelector } from "react-redux";
+// import { useParams } from "react-router-dom";
+
+// const AiConsultChannelPage = () => {
+//   const { uname } = useParams();
+//   const dispatch = useDispatch();
+
+//   const defaultSrc = useSelector((state) => state.aiConsult.audio.defaultSrc);
+//   const src = useSelector((state) => state.aiConsult.audio.src);
+
+//   const isLoading = useSelector(
+//     (state) => state.aiConsult.audio.upload.isLoading
+//   );
+
+//   const handleVideoEnd = () => {
+//     dispatch(clearAudioSrc());
+//   };
+
+//   return (
+//     <Box width="100%" height="100vh">
+//       {/* remote user */}
+//       {
+//         <Box
+//           width="100%"
+//           height="90%"
+//           display="flex"
+//           justifyContent="center"
+//           alignItems="center"
+//         >
+//           {src === "" || isLoading ? (
+//             <Box
+//               component="video"
+//               width="100%"
+//               height="100%"
+//               src={defaultSrc}
+//               loop={true}
+//               autoPlay
+//               onEnded={handleVideoEnd}
+//             />
+//           ) : (
+//             <Box
+//               component="video"
+//               width="100%"
+//               height="100%"
+//               src={src}
+//               autoPlay
+//               onEnded={handleVideoEnd}
+//             />
+//           )}
+//         </Box>
+//       }
+
+//       <Box
+//         sx={{
+//           position: "absolute",
+//           right: 0,
+//           bottom: "10%",
+//           backgroundColor: "blue",
+//           width: { xs: "200px", md: "320px" },
+//           height: { xs: "120px", md: "200px" },
+//         }}
+//       >
+//         <LocalUser />
+//       </Box>
+//       <Box
+//         display="flex"
+//         justifyContent="center"
+//         alignItems="center"
+//         height="10%"
+//         borderTop={1}
+//         borderColor={"#ccc"}
+//       >
+//         <AudioRecorder uname={uname} />
+//       </Box>
+//       {/* <UploadInfoModal /> */}
+//     </Box>
+//   );
+// };
+
 import { AudioRecorder, LocalUser } from "@components/index";
 import { Box } from "@mui/material";
-import { clearAudioSrc } from "@store/ai/aiConsultSlice"; // Adjust the import path accordingly
+import { clearAudioSrc } from "@store/ai/aiConsultSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-
+import { useEffect, useState } from "react";
 const AiConsultChannelPage = () => {
   const { uname } = useParams();
   const dispatch = useDispatch();
-
   const defaultSrc = useSelector((state) => state.aiConsult.audio.defaultSrc);
   const src = useSelector((state) => state.aiConsult.audio.src);
-
   const isLoading = useSelector(
     (state) => state.aiConsult.audio.upload.isLoading
   );
-
+  const [showNewVideo, setShowNewVideo] = useState(false);
+  useEffect(() => {
+    if (src && !isLoading) {
+      setShowNewVideo(true);
+    }
+  }, [src, isLoading]);
   const handleVideoEnd = () => {
     dispatch(clearAudioSrc());
+    setShowNewVideo(false);
   };
-
   return (
-    <Box width="100%" height="100vh">
-      {/* remote user */}
-      {
+    <Box width="100%" height="100vh" position="relative">
+      {/* Video container */}
+      <Box
+        width="100%"
+        height="90%"
+        position="relative"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        {/* Default video */}
         <Box
+          component="video"
           width="100%"
-          height="90%"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
-          {src === "" || isLoading ? (
-            <Box
-              component="video"
-              width="100%"
-              height="100%"
-              src={defaultSrc}
-              loop={true}
-              autoPlay
-              onEnded={handleVideoEnd}
-            />
-          ) : (
-            <Box
-              component="video"
-              width="100%"
-              height="100%"
-              src={src}
-              autoPlay
-              onEnded={handleVideoEnd}
-            />
-          )}
-        </Box>
-      }
-
+          height="100%"
+          src={defaultSrc}
+          loop
+          autoPlay
+          muted
+          sx={{ position: "absolute", top: 0, left: 0 }}
+        />
+        {/* New video */}
+        {showNewVideo && (
+          <Box
+            component="video"
+            width="100%"
+            height="100%"
+            src={src}
+            autoPlay
+            onEnded={handleVideoEnd}
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              transition: "opacity 0.5s ease-in-out",
+              opacity: 1,
+            }}
+          />
+        )}
+      </Box>
+      {/* Local user video */}
       <Box
         sx={{
           position: "absolute",
@@ -65,6 +158,7 @@ const AiConsultChannelPage = () => {
       >
         <LocalUser />
       </Box>
+      {/* Audio recorder */}
       <Box
         display="flex"
         justifyContent="center"
@@ -75,9 +169,7 @@ const AiConsultChannelPage = () => {
       >
         <AudioRecorder uname={uname} />
       </Box>
-      {/* <UploadInfoModal /> */}
     </Box>
   );
 };
-
 export default AiConsultChannelPage;
--- src/store/ai/aiConsultSlice.js
+++ src/store/ai/aiConsultSlice.js
@@ -1,3 +1,89 @@
+// import { ruverseClient } from "@apis/ruverse";
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   audio: {
+//     defaultSrc: "https://ruverse.snu.ac.kr/video/default.mp4",
+//     src: "",
+//     upload: {
+//       error: null,
+//       isError: false,
+//       isSuccess: false,
+//       isLoading: false,
+//     },
+//     current: 1,
+//   },
+//   modal: {
+//     open: false,
+//     modalType: "",
+//     message: null,
+//   },
+// };
+
+// export const uploadRequest = createAsyncThunk(
+//   "asyncThunk/uploadAudioRequest",
+//   async (audioForm) => {
+//     const response = await ruverseClient.post(
+//       "/counseling/get_response",
+//       audioForm,
+//       {
+//         headers: {
+//           "Content-Type": "multipart/form-data",
+//         },
+//       }
+//     );
+//     const data = response.data;
+
+//     return data;
+//   }
+// );
+
+// export const aiConsultSlice = createSlice({
+//   name: "aiConsultSlice",
+//   initialState,
+//   reducers: {
+//     closeModal: (state) => {
+//       state.audio = initialState.audio;
+//       state.audio.upload = initialState.audio.upload;
+//     },
+//     clearAudioSrc: (state) => {
+//       state.audio.src = "";
+//     },
+//     setAudioSrc: (state, action) => {
+//       state.audio.src = action.payload;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder.addCase(uploadRequest.pending, (state) => {
+//       state.audio.upload.isLoading = true;
+//     });
+//     builder.addCase(uploadRequest.fulfilled, (state, action) => {
+//       state.audio.upload.isSuccess = true;
+//       state.audio.upload.isLoading = false;
+//       state.audio.upload.isError = false;
+//       state.audio.upload.error = null;
+
+//       state.audio.current += 1;
+//       state.audio.src = action.payload;
+//       state.modal.open = true;
+//       state.modal.message = "요청 성공";
+//     });
+//     builder.addCase(uploadRequest.rejected, (state, action) => {
+//       state.audio.upload.isSuccess = false;
+//       state.audio.upload.isLoading = false;
+//       state.audio.upload.isError = true;
+//       state.audio.upload.error = action.error.message;
+//       state.modal.message = "요청 실패";
+//       state.modal.open = true;
+//     });
+//   },
+// });
+
+// export const { clearAudioSrc, closeModal, setAudioSrc } =
+//   aiConsultSlice.actions;
+
+// export default aiConsultSlice.reducer;
+
 import { ruverseClient } from "@apis/ruverse";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
@@ -43,7 +129,7 @@ export const aiConsultSlice = createSlice({
   initialState,
   reducers: {
     closeModal: (state) => {
-      state.audio = initialState.audio;
+      state.modal = initialState.modal;
       state.audio.upload = initialState.audio.upload;
     },
     clearAudioSrc: (state) => {
