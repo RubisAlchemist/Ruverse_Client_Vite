cdee032aa028180659fbc5a91d2ec091dd60d70c
yongun2 <djkye0001@naver.com>
2024-07-30 12:32:05 +0900

    default 동영상 추가

--- src/pages/ruverse-ai/AiConsultChannelPage.jsx
+++ src/pages/ruverse-ai/AiConsultChannelPage.jsx
@@ -1,84 +1,15 @@
-// import { AudioRecorder, LocalUser, UploadInfoModal } from "@components/index";
-// import { Box, CircularProgress } from "@mui/material";
-// import { useSelector } from "react-redux";
-// import { useParams } from "react-router-dom";
-
-// const AiConsultChannelPage = () => {
-//   const { uname } = useParams();
-
-//   const src = useSelector((state) => state.aiConsult.audio.src);
-//   console.log(`src change ${src}`);
-
-//   const isLoading = useSelector(
-//     (state) => state.aiConsult.audio.upload.isLoading
-//   );
-
-//   return (
-//     <Box width="100%" height="100vh">
-//       {/* remote user */}
-//       <Box
-//         width="100%"
-//         height="90%"
-//         display="flex"
-//         justifyContent="center"
-//         alignItems="center"
-//       >
-//         {isLoading ? (
-//           <CircularProgress />
-//         ) : (
-//           <Box
-//             component="video"
-//             width="100%"
-//             height="100%"
-//             src={src}
-//             autoPlay
-//           />
-//         )}
-//       </Box>
-
-//       <Box
-//         sx={{
-//           position: "absolute",
-//           right: 0,
-//           bottom: 0 + "10%",
-//           backgroundColor: "blue",
-//           width: { xs: "200px", md: "320px" },
-//           height: { xs: "120px", md: "200px" },
-//         }}
-//       >
-//         <LocalUser />
-//       </Box>
-//       <Box
-//         display="flex"
-//         justifyContent="center"
-//         alignItems="center"
-//         height="10%"
-//         borderTop={1}
-//         borderColor={"#ccc"}
-//       >
-//         <AudioRecorder uname={uname} />
-//       </Box>
-//       <UploadInfoModal />
-//     </Box>
-//   );
-// };
-
-// export default AiConsultChannelPage;
-
 import { AudioRecorder, LocalUser } from "@components/index";
-import { Box, CircularProgress } from "@mui/material";
-import { useSelector, useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
 import { clearAudioSrc } from "@store/ai/aiConsultSlice"; // Adjust the import path accordingly
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const AiConsultChannelPage = () => {
   const { uname } = useParams();
   const dispatch = useDispatch();
 
+  const defaultSrc = useSelector((state) => state.aiConsult.audio.defaultSrc);
   const src = useSelector((state) => state.aiConsult.audio.src);
-  const isLoading = useSelector(
-    (state) => state.aiConsult.audio.upload.isLoading
-  );
 
   const handleVideoEnd = () => {
     dispatch(clearAudioSrc());
@@ -87,26 +18,35 @@ const AiConsultChannelPage = () => {
   return (
     <Box width="100%" height="100vh">
       {/* remote user */}
-      <Box
-        width="100%"
-        height="90%"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        {isLoading ? (
-          <CircularProgress />
-        ) : (
-          <Box
-            component="video"
-            width="100%"
-            height="100%"
-            src={src}
-            autoPlay
-            onEnded={handleVideoEnd}
-          />
-        )}
-      </Box>
+      {
+        <Box
+          width="100%"
+          height="90%"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          {src === "" ? (
+            <Box
+              component="video"
+              width="100%"
+              height="100%"
+              src={defaultSrc}
+              autoPlay
+              onEnded={handleVideoEnd}
+            />
+          ) : (
+            <Box
+              component="video"
+              width="100%"
+              height="100%"
+              src={src}
+              autoPlay
+              onEnded={handleVideoEnd}
+            />
+          )}
+        </Box>
+      }
 
       <Box
         sx={{
