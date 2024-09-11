69aa3e1fa5b2274d08557c6e4748d46fb16432a7
unknown <xognl1124@seoul.ac.kr>
2024-09-11 12:49:12 +0900

    package update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import commonjs from "@rollup/plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs({
+      include: /node_modules/, // node_modules 내부의 CommonJS 모듈 처리
+    }),
+  ],
   server: {
     proxy: {
       "/video": {
@@ -33,7 +39,7 @@ export default defineConfig({
   },
   build: {
     commonjsOptions: {
-      include: [/@ffmpeg\/ffmpeg/, /@ffmpeg\/util/],
+      include: [/@ffmpeg\/ffmpeg/, /@ffmpeg\/util/, /node_modules/],
     },
   },
 });
