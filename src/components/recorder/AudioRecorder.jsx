85b4c12911f31bdfc2d6f687b5f5dc2490b37e02
RUBIS <id@rubis.snu.ac.kr>
2024-10-17 22:32:15 +0900

    profiling

--- src/components/recorder/AudioRecorder.jsx
+++ src/components/recorder/AudioRecorder.jsx
@@ -1,4 +1,164 @@
-// // 클레온 아바타 하단바 디자인 적용 버전
+// // // 클레온 아바타 하단바 디자인 적용 버전
+
+// import { Button, Typography } from "@mui/material";
+// import {
+//   clearAudioSrc,
+//   uploadRequest,
+//   setNotePlaying,
+// } from "@store/ai/aiConsultSlice";
+// import PropTypes from "prop-types";
+// import { useReactMediaRecorder } from "react-media-recorder";
+// import { useDispatch, useSelector } from "react-redux";
+
+// // Icon import
+// import Mic from "@assets/images/mic_blue.png";
+// import Pause from "@assets/images/pause_blue.png";
+
+// // for profiling
+// function saveTimestampsToCSV(timestamps) {
+//   const fields = ["requestSentTime", "firstVideoPlayedTime"];
+//   // Create CSV header and content
+//   const csvRows = [];
+//   csvRows.push(fields.join(",")); // Add header
+//   csvRows.push(
+//     [timestamps.requestSentTime, timestamps.firstVideoPlayedTime].join(",")
+//   ); // Add row
+//   // Convert CSV array to a Blob
+//   const csvContent = csvRows.join("\n");
+//   const blob = new Blob([csvContent], { type: "text/csv" });
+//   // Create a downloadable link for the CSV
+//   const url = window.URL.createObjectURL(blob);
+//   const link = document.createElement("a");
+//   link.href = url;
+//   link.download = "timestamps_profiling_0911_requestSent.csv";
+//   // Programmatically click the link to trigger the download
+//   document.body.appendChild(link);
+//   link.click();
+//   // Cleanup
+//   document.body.removeChild(link);
+//   window.URL.revokeObjectURL(url);
+// }
+// //
+
+// const AudioRecorder = ({
+//   uname,
+//   phoneNumber,
+//   selectedAvatar,
+//   disabled,
+//   onRecordingStart,
+// }) => {
+//   const current = useSelector((state) => state.aiConsult.audio.current);
+//   const dispatch = useDispatch();
+
+//   const { status, startRecording, stopRecording } = useReactMediaRecorder({
+//     audio: true,
+//     blobPropertyBag: {
+//       type: "audio/wav",
+//     },
+//     onStart: () => {
+//       console.log(`[RECORDER] audio record start = ${status}`);
+//       onRecordingStart();
+//     },
+//     onStop: async (url, blob) => {
+//       const requestSentTime = Date.now(); // profiling
+//       const timestamps = { requestSentTime, firstVideoPlayedTime: -1 };
+//       // saveTimestampsToCSV(timestamps);
+//       console.log("대답끝내기: ", requestSentTime);
+//       console.log("[RECORDER] audio record stop");
+//       console.log(blob);
+
+//       const formData = new FormData();
+//       formData.append("audio", blob, `${uname}_audio_${current}.wav`);
+//       formData.append("uname", uname);
+//       formData.append("phoneNumber", phoneNumber);
+//       formData.append("selectedAvatar", selectedAvatar);
+//       console.log(formData.get("audio"));
+//       console.log(formData.get("uname"));
+//       console.log(formData.get("phoneNumber"));
+//       console.log(formData.get("selectedAvatar"));
+
+//       dispatch(clearAudioSrc());
+//       dispatch(uploadRequest(formData));
+//       dispatch(setNotePlaying());
+//     },
+//   });
+
+//   return (
+//     <>
+//       {status === "recording" ? (
+//         <Button
+//           onClick={stopRecording}
+//           disabled={disabled}
+//           sx={{
+//             display: "flex",
+//             flexDirection: "column",
+//             alignItems: "center",
+//             padding: 0,
+//             // width: { xs: "30px", sm: "40px", md: "50px", lg: "60px" },  // 반응형 너비
+//             height: { xs: "30px", sm: "40px", md: "50px", lg: "60px" }, // 반응형 높이
+//           }}
+//         >
+//           <img
+//             src={Pause}
+//             alt="pause icon"
+//             style={{
+//               width: "auto", // 부모 버튼의 크기에 맞춰 이미지 크기 조정
+//               height: "100%",
+//             }}
+//           />
+//           <Typography
+//             sx={{
+//               fontSize: { xs: "10px", sm: "12px", md: "14px", lg: "16px" }, // 반응형 폰트 크기
+//               fontWeight: "800",
+//             }}
+//           >
+//             말 끝내기
+//           </Typography>
+//         </Button>
+//       ) : (
+//         <Button
+//           onClick={startRecording}
+//           disabled={disabled}
+//           sx={{
+//             display: "flex",
+//             flexDirection: "column",
+//             alignItems: "center",
+//             padding: 0,
+//             // width: { xs: "30px", sm: "40px", md: "50px", lg: "60px" },  // 반응형 너비
+//             height: { xs: "30px", sm: "40px", md: "50px", lg: "60px" }, // 반응형 높이
+//           }}
+//         >
+//           <img
+//             src={Mic}
+//             alt="mic icon"
+//             style={{
+//               width: "auto", // 부모 버튼의 크기에 맞춰 이미지 크기 조정
+//               height: "100%",
+//             }}
+//           />
+//           <Typography
+//             sx={{
+//               fontSize: { xs: "10px", sm: "12px", md: "14px", lg: "16px" }, // 반응형 폰트 크기
+//               fontWeight: "800",
+//             }}
+//           >
+//             말 시작하기
+//           </Typography>
+//         </Button>
+//       )}
+//     </>
+//   );
+// };
+
+// AudioRecorder.propTypes = {
+//   uname: PropTypes.string,
+//   phoneNumber: PropTypes.string,
+//   selectedAvatar: PropTypes.string,
+//   disabled: PropTypes.bool,
+//   onRecordingStart: PropTypes.func,
+// };
+
+// export default AudioRecorder;
 
 import { Button, Typography } from "@mui/material";
 import {
@@ -14,38 +174,13 @@ import { useDispatch, useSelector } from "react-redux";
 import Mic from "@assets/images/mic_blue.png";
 import Pause from "@assets/images/pause_blue.png";
 
-// for profiling
-function saveTimestampsToCSV(timestamps) {
-  const fields = ["requestSentTime", "firstVideoPlayedTime"];
-  // Create CSV header and content
-  const csvRows = [];
-  csvRows.push(fields.join(",")); // Add header
-  csvRows.push(
-    [timestamps.requestSentTime, timestamps.firstVideoPlayedTime].join(",")
-  ); // Add row
-  // Convert CSV array to a Blob
-  const csvContent = csvRows.join("\n");
-  const blob = new Blob([csvContent], { type: "text/csv" });
-  // Create a downloadable link for the CSV
-  const url = window.URL.createObjectURL(blob);
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = "timestamps_profiling_0911_requestSent.csv";
-  // Programmatically click the link to trigger the download
-  document.body.appendChild(link);
-  link.click();
-  // Cleanup
-  document.body.removeChild(link);
-  window.URL.revokeObjectURL(url);
-}
-//
-
 const AudioRecorder = ({
   uname,
   phoneNumber,
   selectedAvatar,
   disabled,
   onRecordingStart,
+  onRecordingStop, // 추가된 부분
 }) => {
   const current = useSelector((state) => state.aiConsult.audio.current);
   const dispatch = useDispatch();
@@ -60,12 +195,15 @@ const AudioRecorder = ({
       onRecordingStart();
     },
     onStop: async (url, blob) => {
-      const requestSentTime = Date.now(); // profiling
-      const timestamps = { requestSentTime, firstVideoPlayedTime: -1 };
-      // saveTimestampsToCSV(timestamps);
+      const requestSentTime = Date.now(); // 타임스탬프 기록
       console.log("대답끝내기: ", requestSentTime);
       console.log("[RECORDER] audio record stop");
       console.log(blob);
+
+      // onRecordingStop 콜백 호출
+      if (onRecordingStop) {
+        onRecordingStop(requestSentTime);
+      }
 
       const formData = new FormData();
       formData.append("audio", blob, `${uname}_audio_${current}.wav`);
@@ -156,6 +294,7 @@ AudioRecorder.propTypes = {
   selectedAvatar: PropTypes.string,
   disabled: PropTypes.bool,
   onRecordingStart: PropTypes.func,
+  onRecordingStop: PropTypes.func, // 추가된 부분
 };
 
 export default AudioRecorder;
--- src/pages/ruverse-ai/AiConsultChannelPage.jsx
+++ src/pages/ruverse-ai/AiConsultChannelPage.jsx
@@ -5,7 +5,12 @@
 //   SeamlessVideoPlayer,
 // } from "@components/index";
 // import { Button, Box, Fade, CircularProgress } from "@mui/material";
-// import { clearAudioSrc, setGreetingsPlayed } from "@store/ai/aiConsultSlice";
+// import {
+//   clearAudioSrc,
+//   setGreetingsPlayed,
+//   setNotePlaying,
+//   clearNotePlaying,
+// } from "@store/ai/aiConsultSlice";
 // import { useDispatch, useSelector } from "react-redux";
 // import { useParams, useLocation, useNavigate } from "react-router-dom";
 
@@ -41,12 +46,13 @@
 //   // Retrieve all sources from the Redux store
 //   const audioSources = useSelector((state) => state.aiConsult.audio);
 
-//   console.log("audioSource: ", audioSources[selectedAvatar]);
+//   // console.log("audioSource: ", audioSources[selectedAvatar]);
 
 //   // Select the appropriate sources based on selectedAvatar
 //   const defaultSrc = audioSources[selectedAvatar]?.defaultSrc;
 //   const greetingsSrc = audioSources[selectedAvatar]?.greetingsSrc;
 //   const errorSrc = audioSources[selectedAvatar]?.errorSrc;
+//   const noteSrc = audioSources[selectedAvatar]?.noteSrc;
 
 //   const src = useSelector((state) => state.aiConsult.audio.src);
 //   const isGreetingsPlaying = useSelector(
@@ -54,6 +60,9 @@
 //   );
 //   const isUploading = useSelector(
 //     (state) => state.aiConsult.audio.upload.isLoading
+//   );
+//   const isNotePlaying = useSelector(
+//     (state) => state.aiConsult.audio.isNotePlaying
 //   );
 
 //   let BackgroundImage;
@@ -126,6 +135,7 @@
 //     console.log("State change detected", {
 //       isGreetingsPlaying,
 //       greetingsSrc,
+//       isNotePlaying,
 //       src,
 //       errorSrc,
 //       isSeamlessPlaying,
@@ -151,6 +161,7 @@
 //       src &&
 //       !isSeamlessPlaying &&
 //       !isGreetingsPlaying &&
+//       !overlayVideo &&
 //       src !== "error"
 //     ) {
 //       console.log("Starting seamless video playback");
@@ -159,7 +170,12 @@
 //       setIsLoading(true);
 //     }
 //     // 초기 상태로 리셋
-//     else if (!src && !isGreetingsPlaying && !isSeamlessPlaying) {
+//     else if (
+//       !src &&
+//       !isGreetingsPlaying &&
+//       !isSeamlessPlaying &&
+//       !isNotePlaying
+//     ) {
 //       console.log("Resetting to default state");
 //       setOverlayVideo(null);
 //       setIsSeamlessPlaying(false);
@@ -167,8 +183,10 @@
 //   }, [
 //     isGreetingsPlaying,
 //     src,
+//     isNotePlaying,
 //     greetingsSrc,
 //     errorSrc, // errorSrc를 의존성 배열에 추가
+//     noteSrc,
 //     isSeamlessPlaying,
 //     overlayVideo,
 //     isSeamlessLoading,
@@ -180,13 +198,16 @@
 //     if (isGreetingsPlaying) {
 //       console.log("Greeting video finished");
 //       dispatch(setGreetingsPlayed());
+//     } else if (isNotePlaying) {
+//       console.log("Note video finished");
+//       dispatch(clearNotePlaying());
 //     } else {
 //       console.log("Regular overlay video finished");
 //       dispatch(clearAudioSrc());
 //     }
 //     setOverlayVideo(null);
 //     setIsAnswerButtonEnabled(true);
-//   }, [dispatch, isGreetingsPlaying]);
+//   }, [dispatch, isGreetingsPlaying, isNotePlaying]);
 
 //   const handleSeamlessVideoEnd = useCallback(() => {
 //     console.log("Seamless video playback ended");
@@ -200,8 +221,12 @@
 //     console.log(src);
 //     setIsLoading(false);
 //     setIsAnswerButtonEnabled(false);
-//   }, [src]);
-
+//     if (isNotePlaying) {
+//       console.log("Stopping note video as seamless video is starting");
+//       dispatch(clearNotePlaying());
+//       setOverlayVideo(null);
+//     }
+//   }, [src, dispatch, isNotePlaying]);
 //   const handleAllVideosEnded = useCallback(() => {
 //     console.log("All seamless videos have ended");
 //     setIsSeamlessPlaying(false);
@@ -443,6 +468,433 @@
 
 // export default AiConsultChannelPage;
 
+// import React, { useState, useEffect, useRef, useCallback } from "react";
+// import {
+//   AudioRecorder,
+//   LocalUser,
+//   SeamlessVideoPlayer,
+// } from "@components/index";
+// import { Button, Box, Fade, CircularProgress } from "@mui/material";
+// import {
+//   clearAudioSrc,
+//   setGreetingsPlayed,
+//   setNotePlaying,
+//   clearNotePlaying,
+// } from "@store/ai/aiConsultSlice";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useParams, useLocation, useNavigate } from "react-router-dom";
+
+// // 아이콘 및 이미지 임포트
+// import Exit from "@assets/images/exit.png";
+// import Describe1Image from "@assets/images/describe1.png";
+// import Describe2Image from "@assets/images/describe2.png";
+// import BackgroundImage_sonny from "@assets/images/background_sonny.png";
+// import BackgroundImage_jennie from "@assets/images/background_jennie.png";
+
+// const useQuery = () => {
+//   return new URLSearchParams(useLocation().search);
+// };
+
+// const AiConsultChannelPage = () => {
+//   const { uname } = useParams();
+//   const query = useQuery();
+//   const phoneNumber = query.get("phoneNumber");
+//   const selectedAvatar = query.get("selectedAvatar");
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   // 상태 변수들
+//   const [overlayVideo, setOverlayVideo] = useState(null);
+//   const [isSeamlessPlaying, setIsSeamlessPlaying] = useState(false);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isAnswerButtonEnabled, setIsAnswerButtonEnabled] = useState(true);
+//   const [showInstruction, setShowInstruction] = useState(true);
+//   const [isSeamlessLoading, setIsSeamlessLoading] = useState(false);
+
+//   const greetingsVideoRef = useRef(null);
+
+//   // Redux 상태 가져오기
+//   const audioSources = useSelector((state) => state.aiConsult.audio);
+//   const src = useSelector((state) => state.aiConsult.audio.src);
+//   const isGreetingsPlaying = useSelector(
+//     (state) => state.aiConsult.audio.isGreetingsPlaying
+//   );
+//   const isNotePlaying = useSelector(
+//     (state) => state.aiConsult.audio.isNotePlaying
+//   );
+//   const isUploading = useSelector(
+//     (state) => state.aiConsult.audio.upload.isLoading
+//   );
+
+//   // 선택된 아바타에 따른 소스 가져오기
+//   const defaultSrc = audioSources[selectedAvatar]?.defaultSrc;
+//   const greetingsSrc = audioSources[selectedAvatar]?.greetingsSrc;
+//   const errorSrc = audioSources[selectedAvatar]?.errorSrc;
+//   const noteSrc = audioSources[selectedAvatar]?.noteSrc;
+
+//   // 배경 이미지 설정
+//   let BackgroundImage;
+//   if (selectedAvatar === "sonny") {
+//     BackgroundImage = BackgroundImage_sonny;
+//   } else if (selectedAvatar === "jennie") {
+//     BackgroundImage = BackgroundImage_jennie;
+//   } else {
+//     BackgroundImage = BackgroundImage_sonny;
+//   }
+
+//   // 상담 종료 핸들러
+//   const handleEndConsultation = useCallback(() => {
+//     dispatch(clearAudioSrc());
+//     navigate("/ai-consultEntry", { replace: true });
+//     window.location.reload();
+//   }, [navigate, dispatch]);
+
+//   // 종료 버튼 클릭 핸들러
+//   const handleExitClick = useCallback(
+//     (e) => {
+//       e.preventDefault();
+//       e.stopPropagation();
+//       handleEndConsultation();
+//     },
+//     [handleEndConsultation]
+//   );
+
+//   // 페이지 새로고침 방지 핸들러
+//   const handleRefresh = useCallback((e) => {
+//     e.preventDefault();
+//     const confirmRefresh = window.confirm("페이지를 새로고침하시겠습니까?");
+//     if (confirmRefresh) {
+//       window.location.reload();
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     const handleKeyPress = (e) => {
+//       if (e.key === "F5" || (e.ctrlKey && e.key === "r")) {
+//         handleRefresh(e);
+//       }
+//     };
+//     window.addEventListener("keydown", handleKeyPress);
+//     return () => {
+//       window.removeEventListener("keydown", handleKeyPress);
+//     };
+//   }, [handleRefresh]);
+
+//   // 상태 변화에 따른 비디오 재생 로직
+//   useEffect(() => {
+//     if (!overlayVideo) {
+//       if (isGreetingsPlaying && greetingsSrc) {
+//         console.log("인사말 비디오 재생");
+//         setOverlayVideo(greetingsSrc);
+//         setIsSeamlessPlaying(false);
+//       } else if (src === "error") {
+//         console.log("에러 비디오 재생");
+//         setOverlayVideo(errorSrc);
+//         setIsSeamlessPlaying(false);
+//         // 에러 비디오 재생을 표시하기 위해 상태 설정
+//       } else if (isNotePlaying && noteSrc) {
+//         console.log("노트 비디오 재생");
+//         setOverlayVideo(noteSrc);
+//       }
+//     }
+
+//     if (src && !isSeamlessPlaying && src !== "error") {
+//       console.log("시작하기 seamless 비디오 재생");
+//       setIsSeamlessPlaying(true);
+//       setIsLoading(true);
+//     }
+//   }, [
+//     overlayVideo,
+//     isGreetingsPlaying,
+//     greetingsSrc,
+//     src,
+//     errorSrc,
+//     isNotePlaying,
+//     noteSrc,
+//     isSeamlessPlaying,
+//     dispatch,
+//   ]);
+
+//   // overlay 비디오 종료 핸들러
+//   const handleOverlayVideoEnd = useCallback(() => {
+//     console.log("Overlay 비디오 종료");
+//     if (isGreetingsPlaying) {
+//       dispatch(setGreetingsPlayed());
+//     } else if (isNotePlaying) {
+//       dispatch(clearNotePlaying());
+//     } else if (src === "error") {
+//       console.log("에러 비디오 재생 종료");
+//       dispatch(clearAudioSrc()); // src를 초기화하여 에러 비디오가 다시 재생되지 않도록 함
+//     }
+//     setOverlayVideo(null);
+//     setIsAnswerButtonEnabled(true);
+//   }, [dispatch, isGreetingsPlaying, isNotePlaying, src]);
+
+//   // seamless 비디오 핸들러들
+//   const handleSeamlessVideoEnd = useCallback(() => {
+//     console.log("Seamless 비디오 재생 종료");
+//     setIsSeamlessPlaying(false);
+//     setIsLoading(false);
+//     dispatch(clearAudioSrc());
+//   }, [dispatch]);
+
+//   const handleSeamlessVideoStart = useCallback(() => {
+//     console.log("Seamless 비디오 재생 시작");
+//     setIsLoading(false);
+//     setIsAnswerButtonEnabled(false);
+//   }, []);
+
+//   const handleAllVideosEnded = useCallback(() => {
+//     console.log("모든 Seamless 비디오 종료");
+//     setIsSeamlessPlaying(false);
+//     setIsLoading(false);
+//     dispatch(clearAudioSrc());
+//     setIsAnswerButtonEnabled(true);
+//   }, [dispatch]);
+
+//   const handleGreetingsVideoPlay = () => {
+//     console.log("인사말 비디오 재생 시작");
+//   };
+
+//   const handleGreetingsVideoError = (e) => {
+//     console.error("인사말 비디오 재생 오류:", e);
+//   };
+
+//   const handleRecordingStart = () => {
+//     console.log("녹음 시작");
+//     setShowInstruction(false);
+//   };
+
+//   const handleRecordingStop = () => {
+//     console.log("녹음 종료");
+//   };
+
+//   return (
+//     <Box width="100%" height="100vh">
+//       <Box
+//         width="100%"
+//         height="90%"
+//         display="flex"
+//         justifyContent="center"
+//         alignItems="center"
+//         position="relative"
+//       >
+//         {/* 배경 이미지 */}
+//         <Box
+//           component="img"
+//           src={BackgroundImage}
+//           alt="Background"
+//           position="absolute"
+//           height="100%"
+//           objectFit="cover"
+//           zIndex={0}
+//           sx={{
+//             display: { xs: "none", md: "block" },
+//             border: "none",
+//           }}
+//         />
+
+//         {/* default 비디오 */}
+//         <Box
+//           component="video"
+//           width="100%"
+//           height="100%"
+//           top={0}
+//           left={0}
+//           src={defaultSrc}
+//           loop
+//           autoPlay
+//           muted
+//           position="relative"
+//           zIndex={1}
+//         />
+
+//         {/* overlay 비디오 (greetings, note, error 등) */}
+//         {overlayVideo && (
+//           <Fade in={true}>
+//             <Box
+//               position="absolute"
+//               top={0}
+//               left={0}
+//               width="100%"
+//               height="100%"
+//               component="video"
+//               ref={greetingsVideoRef}
+//               src={overlayVideo}
+//               autoPlay
+//               onEnded={handleOverlayVideoEnd}
+//               onPlay={handleGreetingsVideoPlay}
+//               onError={handleGreetingsVideoError}
+//               zIndex={2}
+//             />
+//           </Fade>
+//         )}
+
+//         {/* seamless 비디오 */}
+//         {isSeamlessPlaying && (
+//           <Box
+//             position="absolute"
+//             top={0}
+//             left={0}
+//             width="100%"
+//             height="100%"
+//             zIndex={3}
+//             sx={{ border: "none" }}
+//           >
+//             <SeamlessVideoPlayer
+//               initialVideoUrl={src}
+//               isVisible={isSeamlessPlaying}
+//               onEnded={handleAllVideosEnded}
+//               onStart={handleSeamlessVideoStart}
+//               onAllVideosEnded={handleAllVideosEnded}
+//             />
+//           </Box>
+//         )}
+
+//         {/* 로딩 표시 */}
+//         {isLoading && (
+//           <Box
+//             position="absolute"
+//             top={0}
+//             left={0}
+//             width="100%"
+//             height="100%"
+//             display="flex"
+//             justifyContent="center"
+//             alignItems="center"
+//             bgcolor="transparent"
+//           >
+//             <CircularProgress />
+//           </Box>
+//         )}
+
+//         {isSeamlessLoading && (
+//           <Box
+//             position="absolute"
+//             top={0}
+//             left={0}
+//             width="100%"
+//             height="100%"
+//             bgcolor="transparent"
+//             zIndex={4}
+//           />
+//         )}
+//       </Box>
+
+//       {/* 로컬 유저 비디오 */}
+//       <Box
+//         position="absolute"
+//         zIndex={2}
+//         right={0}
+//         bottom={"10%"}
+//         width={{ xs: "200px", md: "320px" }}
+//         height={{ xs: "120px", md: "200px" }}
+//       >
+//         <LocalUser />
+//       </Box>
+
+//       {/* 하단 컨트롤 바 */}
+//       <Box
+//         position="relative"
+//         display="flex"
+//         justifyContent="center"
+//         alignItems="center"
+//         height="10%"
+//         borderTop={1}
+//         borderColor={"#ccc"}
+//       >
+//         {showInstruction && (
+//           <Box
+//             position="absolute"
+//             margin="auto"
+//             display="flex"
+//             sx={{
+//               transform: "translateX(-65%)",
+//               height: { xs: "24px", sm: "40px", md: "50px", lg: "60px" },
+//             }}
+//           >
+//             <img
+//               src={Describe1Image}
+//               alt="describe1"
+//               style={{
+//                 width: "auto",
+//                 height: "100%",
+//               }}
+//             />
+//           </Box>
+//         )}
+
+//         {/* 오디오 레코더 */}
+//         <AudioRecorder
+//           uname={uname}
+//           phoneNumber={phoneNumber}
+//           selectedAvatar={selectedAvatar}
+//           disabled={
+//             isGreetingsPlaying ||
+//             !!overlayVideo ||
+//             isSeamlessPlaying ||
+//             isUploading ||
+//             isLoading ||
+//             !isAnswerButtonEnabled
+//           }
+//           onRecordingStart={handleRecordingStart}
+//           onRecordingStop={handleRecordingStop}
+//         />
+
+//         {/* 종료 버튼 */}
+//         <Box
+//           position="absolute"
+//           right="2px"
+//           display="flex"
+//           alignItems="center"
+//           sx={{
+//             gap: { xs: "2px", sm: "3px", md: "4px", lg: "5px" },
+//           }}
+//         >
+//           {showInstruction && (
+//             <Box
+//               sx={{
+//                 height: { xs: "24px", sm: "40px", md: "50px", lg: "60px" },
+//               }}
+//             >
+//               <img
+//                 src={Describe2Image}
+//                 alt="describe2"
+//                 style={{
+//                   width: "auto",
+//                   height: "100%",
+//                 }}
+//               />
+//             </Box>
+//           )}
+
+//           <Button
+//             onClick={handleExitClick}
+//             sx={{
+//               display: "flex",
+//               justifyContent: "center",
+//               alignItems: "center",
+//               height: { xs: "35px", sm: "45px", md: "55px", lg: "65px" },
+//               minWidth: 0,
+//             }}
+//           >
+//             <img
+//               src={Exit}
+//               alt="exit icon"
+//               style={{
+//                 width: "auto",
+//                 height: "100%",
+//               }}
+//             />
+//           </Button>
+//         </Box>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default AiConsultChannelPage;
+
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import {
   AudioRecorder,
@@ -485,6 +937,7 @@ const AiConsultChannelPage = () => {
   const [isAnswerButtonEnabled, setIsAnswerButtonEnabled] = useState(true);
   const [showInstruction, setShowInstruction] = useState(true);
   const [isSeamlessLoading, setIsSeamlessLoading] = useState(false);
+  const [timestampsArray, setTimestampsArray] = useState([]); // 타임스탬프 저장 배열 추가
 
   const greetingsVideoRef = useRef(null);
 
@@ -524,14 +977,37 @@ const AiConsultChannelPage = () => {
     window.location.reload();
   }, [navigate, dispatch]);
 
+  // CSV 저장 함수
+  const saveTimestampsToCSV = (timestampsArray) => {
+    const fields = ["requestSentTime", "firstVideoPlayedTime"];
+    const csvRows = [];
+    csvRows.push(fields.join(",")); // 헤더 추가
+    timestampsArray.forEach((timestamp) => {
+      csvRows.push(
+        [timestamp.requestSentTime, timestamp.firstVideoPlayedTime].join(",")
+      );
+    });
+    const csvContent = csvRows.join("\n");
+    const blob = new Blob([csvContent], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `timestamps_${Date.now()}.csv`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+  };
+
   // 종료 버튼 클릭 핸들러
   const handleExitClick = useCallback(
     (e) => {
       e.preventDefault();
       e.stopPropagation();
+      saveTimestampsToCSV(timestampsArray); // CSV 저장 함수 호출
       handleEndConsultation();
     },
-    [handleEndConsultation]
+    [handleEndConsultation, timestampsArray]
   );
 
   // 페이지 새로고침 방지 핸들러
@@ -617,6 +1093,14 @@ const AiConsultChannelPage = () => {
     console.log("Seamless 비디오 재생 시작");
     setIsLoading(false);
     setIsAnswerButtonEnabled(false);
+
+    // 타임스탬프 배열의 마지막 객체에 firstVideoPlayedTime 추가
+    setTimestampsArray((prevArray) => {
+      if (prevArray.length === 0) return prevArray;
+      const newArray = [...prevArray];
+      newArray[newArray.length - 1].firstVideoPlayedTime = Date.now();
+      return newArray;
+    });
   }, []);
 
   const handleAllVideosEnded = useCallback(() => {
@@ -640,9 +1124,14 @@ const AiConsultChannelPage = () => {
     setShowInstruction(false);
   };
 
-  const handleRecordingStop = () => {
+  const handleRecordingStop = useCallback((timestamp) => {
     console.log("녹음 종료");
-  };
+    // 타임스탬프 배열에 새로운 객체 추가
+    setTimestampsArray((prevArray) => [
+      ...prevArray,
+      { requestSentTime: timestamp, firstVideoPlayedTime: null },
+    ]);
+  }, []);
 
   return (
     <Box width="100%" height="100vh">
@@ -813,7 +1302,7 @@ const AiConsultChannelPage = () => {
             !isAnswerButtonEnabled
           }
           onRecordingStart={handleRecordingStart}
-          onRecordingStop={handleRecordingStop}
+          onRecordingStop={handleRecordingStop} // 추가된 부분
         />
 
         {/* 종료 버튼 */}
