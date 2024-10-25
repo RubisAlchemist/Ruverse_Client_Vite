5805305cb782dd1b0db3d02399093a22d6a88d43
RUBIS <id@rubis.snu.ac.kr>
2024-10-25 12:11:32 +0900

    magin number 2000

--- src/components/video/SeamlessVideoPlayer.jsx
+++ src/components/video/SeamlessVideoPlayer.jsx
@@ -52,7 +52,7 @@ const SeamlessVideoPlayer = ({
 
     if (index === 0) {
       console.log("sleeping...");
-      await sleep(1400);
+      await sleep(2000);
       console.log("sleep end");
       setIsInitialLoading(false);
     }
